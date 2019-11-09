385aee8debcf7576ce438d4dc65129e4ff32b5f3
Ryan Conn <rconn478@gmail.com>
2019-11-09 13:27:35 -0600

    Made script to make camera follow player

---
+++ Platformer/Assets/Scripts/CameraControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    public Transform player;
+    public float distanceFromPlayer;
+    public float maxDistanceFromPlayer;
+    private Vector3 eye;
+    // Start is called before the first frame update
+    void Start()
+    {
+        eye = new Vector3(0, 0, -distanceFromPlayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerPos = player.position;
+        Vector2 lerpedPosition = Vector2.Lerp(transform.position, playerPos, 0.05f);
+        lerpedPosition = new Vector2(Mathf.Clamp(lerpedPosition.x, playerPos.x - maxDistanceFromPlayer, playerPos.x + maxDistanceFromPlayer),
+                                     Mathf.Clamp(lerpedPosition.y, playerPos.y - maxDistanceFromPlayer, playerPos.y + maxDistanceFromPlayer));
+        /*
+        if ((lerpedPosition - playerPos).magnitude > maxDistanceFromPlayer) {
+            lerpedPosition = playerPos + lerpedPosition.normalized * maxDistanceFromPlayer;
+        }
+        */
+        transform.position = lerpedPosition;
+        transform.position += eye;
+    }
+}
