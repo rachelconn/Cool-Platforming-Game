d77bf074aab2d0b4f1d1af5fe6f334a9bee45f4b
ericweiss98tamu <ericweiss98@tamu.edu>
2019-11-13 17:56:24 -0600

    Scene change button
    Made a script and button to start new game

--- Platformer/Assets/Scripts/NewGame.cs
+++ Platformer/Assets/Scripts/NewGame.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class NewGame : MonoBehaviour
 {
-    public void StartNew()
+    public void NextScene(string scene)
     {
-        Application.LoadLevel("Test");
+        SceneManager.LoadScene(scene);
     }
 }
