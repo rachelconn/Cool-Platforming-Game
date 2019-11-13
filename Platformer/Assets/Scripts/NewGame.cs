66dcfd9ac418b7519baf3726adde4995e69313b5
ericweiss98tamu <ericweiss98@tamu.edu>
2019-11-13 15:42:37 -0600

    Layers and title stuff

---
+++ Platformer/Assets/Scripts/NewGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGame : MonoBehaviour
+{
+    public void StartNew()
+    {
+        Application.LoadLevel("Test");
+    }
+}
