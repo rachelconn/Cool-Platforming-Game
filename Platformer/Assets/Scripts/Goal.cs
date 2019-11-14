987a0bc88f3ea7c0c0e77861e58bb3c5a46415e5
Ryan Conn <rconn478@gmail.com>
2019-11-13 18:24:17 -0600

    Added basic level end functionality

---
+++ Platformer/Assets/Scripts/Goal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    private Stopwatch stopwatch;
+    private bool levelCompleted;
+
+    private void FinishLevel() {
+        stopwatch.Stop();
+        TimeSpan ts = stopwatch.Elapsed;
+        UnityEngine.Debug.Log(String.Format("{0}:{1:00}.{2}", Math.Floor(ts.TotalMinutes), ts.Seconds, ts.Milliseconds));
+        Time.timeScale = 0;
+    }
+
+    void Start()
+    {
+        levelCompleted = false;
+        stopwatch = new Stopwatch();
+        stopwatch.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        UnityEngine.Debug.Log("a");
+        if (!levelCompleted) {
+            levelCompleted = true;
+            FinishLevel();
+        }
+    }
+}
---
+++ Platformer/Assets/Scripts/LevelCompleteControls.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCompleteControls : MonoBehaviour
+{
+    private int selectionNum;
+    private bool LeftPressed;
+    private bool RightPressed;
+    private string[] buttonNames = {"Retry Button", "Continue Button", "Exit Button"};
+    public Transform selection;
+    // don't interpret axis movement as button press unless it goes under the threshold before pressing again
+    public float inputThreshold;
+
+    void ChangeSelection(int amount) {
+        int newSelection = selectionNum + amount;
+        if (newSelection >= 0 && newSelection < buttonNames.Length)
+            selectionNum = newSelection;
+    }
+
+    void Start()
+    {
+        selectionNum = 1;
+    }
+
+    void Update()
+    {
+        // filter input so user has to press left/right every time selection changes
+        float inputDir = Input.GetAxisRaw("Horizontal");
+        if (LeftPressed)
+            if (inputDir > -inputThreshold)
+                LeftPressed = false;
+        if (RightPressed)
+            if (inputDir < inputThreshold)
+                RightPressed = false;
+        if (!LeftPressed && inputDir <= -inputThreshold) {
+            ChangeSelection(-1);
+            LeftPressed = true;
+        }
+        if (!RightPressed && inputDir >= inputThreshold) {
+            ChangeSelection(1);
+            RightPressed = true;
+        }
+
+        // update position of selection
+        Debug.Log(selectionNum);
+        selection.position = transform.Find(buttonNames[selectionNum]).position;
+    }
+}
