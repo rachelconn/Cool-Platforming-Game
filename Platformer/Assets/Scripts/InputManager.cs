d26d746c0b57d0933d7c2a4444dd6054cdf68790
LuisDLF98 <57546220+LuisDLF98@users.noreply.github.com>
2019-11-15 16:27:50 -0600

    Added DontDestroyOnLoad() in Awake()

--- Platformer/Assets/Scripts/InputManager.cs
+++ Platformer/Assets/Scripts/InputManager.cs
@@ -6,6 +6,12 @@ using System.Linq;
 public class InputManager : MonoBehaviour
 {
     private Dictionary<string, KeyCode> keys;
+    
+    // Keep the object between scene changes
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
 
     // Use this for initialization
     void OnEnable()
