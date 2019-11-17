20517c39f594a7ac8af996da1147dc9a1bf25e9e
ericweiss98tamu <ericweiss98@tamu.edu>
2019-11-17 16:24:31 -0600

    Created save functions
    Created save functions and set the buttons to the load.

--- Platformer/Assets/Scripts/Save.cs
+++ Platformer/Assets/Scripts/Save.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
 
 [System.Serializable]
 public class Save
 {
 
-    public int LevelNum = 0;
+    public string levelNum = "0";
+    public string saveFile = "autosave";
 
 }
