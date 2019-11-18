2f9941acafa523115ab67012bedd92b3b50a32e7
Luis De La Fuente <ldelaf98@tamu.edu>
2019-11-18 16:26:51 -0600

    Added a difficulty dropdown menu

---
+++ Platformer/Assets/Scripts/ChangeDifficulty.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeDifficulty : MonoBehaviour
+{
+    private Dropdown myDropDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myDropDown = this.GetComponent<Dropdown>();
+        myDropDown.onValueChanged.AddListener(delegate { ChangeDiff(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeDiff()
+    {
+        if (myDropDown.value == 0)
+        {
+            BarrelSpeed.speed = 6;
+        }
+        else if (myDropDown.value == 1)
+        {
+            BarrelSpeed.speed = 8;
+        }
+        else if (myDropDown.value == 2)
+        {
+            BarrelSpeed.speed = 10;
+        }
+
+        Debug.LogError("BarrelSpeed: " + BarrelSpeed.speed);
+    }
+}
