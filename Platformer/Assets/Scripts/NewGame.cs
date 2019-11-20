f2fbb553e36fffb4c6b8875ae98a008362400f66
Ryan Conn <rconn478@gmail.com>
2019-11-19 19:05:48 -0600

    Fixed new game script

--- Platformer/Assets/Scripts/NewGame.cs
+++ Platformer/Assets/Scripts/NewGame.cs
@@ -5,10 +5,18 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
+
 public class NewGame : MonoBehaviour
 {
+    public GameObject settingsUI;
+
     public void NextScene(string scene)
     {
         SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+
+    public void SettingsScreen() {
+        Instantiate(settingsUI);
+        SceneManager.LoadScene("Settings", LoadSceneMode.Additive);
     }
 }
