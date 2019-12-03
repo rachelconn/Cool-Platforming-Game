ff1203557aa2e73aa6b77e4b333a2587b1f109a7
Kelvin Wang <30770221+kelvinwop@users.noreply.github.com>
2019-12-03 16:59:14 -0600

    capable of playing on contextual reminders

--- Platformer/Assets/Scripts/ContextualReminder.cs
+++ Platformer/Assets/Scripts/ContextualReminder.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[RequireComponent(typeof(AudioSource))]
 public class ContextualReminder : MonoBehaviour
 {
     public string MessageToShow;
     public GameObject MessageBox;
+    public AudioClip narration;
 
+    private AudioSource sPlayer;
     private TMPro.TextMeshProUGUI _textBox;
     private float _displayTime = 0;
     private bool _showing = false;
+    private float cooldownTime = 30f;
+    private float curCooldownTime = 0f; // timer that counts down, ready at 0
+    private bool playable; // if we have a sound clip even
 
     private void Start()
     {
         _textBox = MessageBox.GetComponent<TMPro.TextMeshProUGUI>();
         SetMessage(MessageToShow);
+        sPlayer = GetComponent<AudioSource>();
+        playable = narration != null;
+        if (playable)
+        {
+            sPlayer.clip = narration;
+        }
     }
 
     public void SetMessage(string message)
@@ -38,6 +50,10 @@ public class ContextualReminder : MonoBehaviour
                 _showing = false;
             }
         }
+        if (curCooldownTime > 0f)
+        {
+            curCooldownTime -= Time.deltaTime;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -47,6 +63,11 @@ public class ContextualReminder : MonoBehaviour
             _displayTime = 15f;
             MessageBox.SetActive(true);
             _showing = true;
+
+            if (curCooldownTime <= 0 && playable)
+            {
+                sPlayer.Play();
+            }
         }
     }
 }
