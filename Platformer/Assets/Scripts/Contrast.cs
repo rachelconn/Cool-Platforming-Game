9293069aa0cbaa93bbbe014713158c60f6077a8d
Ryan Conn <rconn478@gmail.com>
2019-12-03 15:32:58 -0600

    Added Contrast
    A ppv game object was added to each scene and a post processing layer was added to each camera in order for the contrast to take affect. I also changed the layout of the settings menus to better fit the contrast slider.

---
+++ Platformer/Assets/Scripts/Contrast.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.Rendering.PostProcessing;
+
+public class Contrast : MonoBehaviour
+{
+    public PostProcessProfile ppp;
+    private ColorGrading color;
+    public static float contrastValue;
+    public Slider contrastSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ppp.TryGetSettings(out color);
+        contrastSlider.value = contrastValue;
+    }
+
+    // Set contrast to the value on the contrast slider
+    void Update()
+    {
+        contrastValue = contrastSlider.value;
+        color.contrast.value = contrastValue;
+    }
+}
